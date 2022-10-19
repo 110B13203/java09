65242526c08fe2f893a86a71a47d70483944fce1
110B13203 <112997321+110B13203@users.noreply.github.com>
2022-10-19 11:07:50 +0800

    Update Class01.java

--- Class01.java
+++ Class01.java
@@ -1,1 +1,29 @@
+package java09;
 
+class CRectangle{
+	int width;
+	int height;
+	public CRectangle() {
+		width=10;
+		height=8;
+		this.width=10;
+		this.height=8;
+	}
+	public CRectangle(int w,int h) {
+		width=w;
+		height=h;
+	}
+	public void show() {
+		System.out.println(width+","+height);
+	}
+}
+public class Class01 {
+
+	public static void main(String[] args) {
+		CRectangle a=new CRectangle();
+		a.show();
+		a=new CRectangle(15,16);
+		a.show();
+	}
+
+}
