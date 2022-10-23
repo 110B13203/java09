5f92702da185179dba1fced7e97260af096d260c
110B13203 <112997321+110B13203@users.noreply.github.com>
2022-10-23 21:13:50 +0800

    Add files via upload

---
+++ Class07.java
@@ -0,0 +1,16 @@
+package java09;
+
+class count{
+	public static void power(int x,int n) {
+		System.out.println((int)Math.pow(x, n));
+	}
+}
+public class Class07 {
+
+	public static void main(String[] args) {
+		count.power(2, 5);
+		count.power(3, 2);
+
+	}
+
+}
